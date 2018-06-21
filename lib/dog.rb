8a167ac62e9619137800020f4a38dd43d219a8c9
Amrita Bhasin <amritawriter7@gmail.com>
2018-06-21 23:22:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+
+class Dog
+  
+# def initialize(bark, sit) 
+#   @bark = bark 
+#   @sit = sit
+# end
+
+def bark 
+  puts "Woof!"
+end 
+
+def sit 
+  puts "The Dog is sitting"
+end 
+end
+
+# def setter_method = new_atribute 
+# @attribute = new_attribute 
+# end 
