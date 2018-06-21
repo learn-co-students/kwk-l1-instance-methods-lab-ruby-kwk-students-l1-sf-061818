49ba418d5b3b67167ea7d14ee038b5e1a7b4b3eb
Mariana <mantaya00@gmail.com>
2018-06-21 23:18:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+class Dog 
+  def instance 
+  @bark puts "Woof!"
+end 
+  def instance 
+    @sit puts "The Dog is sitting"
+  end 
+end 
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
 # person.rb
+class Person
+  def instance (talk, walk)
+    @talk = puts "Hello World!"
+    @walk = puts "The Person is walking"
+  end 
+end 
+
